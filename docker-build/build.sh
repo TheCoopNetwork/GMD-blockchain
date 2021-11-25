0f233db985aa05c43117bd3c116e3fe99f2a4f96
George Asiminei <george.asiminei@gmail.com>
2021-11-26 00:41:27 +0200

    Update build. Cleanup .idea ide file

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -11,8 +11,9 @@ cp -r html /output/
 cp -r lib /output/
 cp -r conf /output/
 #cp -r nxt_db /output/
+mkdir -p /output/logs
 
-jlink --add-modules "java.base,java.compiler,java.desktop,java.instrument,java.logging,java.management,java.naming,java.scripting,java.security.jgss,java.sql,java.transaction.xa,java.xml" --output ../output/jre-for-gmd
+#echo 'jlink --add-modules "java.base,java.compiler,java.desktop,java.instrument,java.logging,java.management,java.naming,java.scripting,java.security.jgss,java.sql,java.transaction.xa,java.xml" --output ../output/jre-for-gmd-win' > /output/get-jre-win.bat
 #echo 'java -cp lib\*;conf;CoopNetwork.jar -Dnxt.runtime.dirProvider=nxt.env.DefaultDirProvider nxt.Nxt' > /output/start.bat
 echo '#!/bin/bash' > /output/start.sh
 echo 'jre-for-gmd/bin/java -jar CoopNetwork.jar -cp lib\* -cp conf -Dnxt.runtime.dirProvider=nxt.env.DefaultDirProvider nxt.Nxt' >> /output/start.sh
